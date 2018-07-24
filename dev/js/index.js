e640f2d68c5de7296d1fbed722895bd0053b1310
Josiah Maina <josiah5115@gmail.com>
2018-07-23 20:38:26 -0600

    store and reducer initialized

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,6 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
 
+
+// Store store all the data for our application;
+//  The data stored in our store/ storage is determined by our reducer;
+// A reducer is a function that says what data we're supposed to store in our store;
+const store = createStore();
 
 ReactDOM.render (<h1>Hey there!</h1>, document.getElementById('root'));
---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function() {
+  return [
+    {
+        id: 1,
+        first: "Joh",
+        last: "Maina",
+        age: 25,
+        description: "Joh is an upcoming developer and movie lover",
+        thumbnail: "http://i.imgur.com/7yUePI.jpg"
+    },
+    {
+        id: 2,
+        first: "Bucky",
+        last: "Roberts",
+        age: 71,
+        description: "Bucky is a React developer and YouTuber",
+        thumbnail: "http://i.imgur.com/52xRIm8.png"
+    },
+    {
+        id: 3,
+        first: "Madison",
+        last: "Williams",
+        age: 20,
+        description: "Madi likes her dog but it is really annoying",
+        thumbnail: "http://i.imgur.com/4EMtxHB.png"
+    }
+  ]
+};
